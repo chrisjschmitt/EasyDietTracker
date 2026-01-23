ec5e52336577fe346c51963c0ba000969491ee90
Christopher Schmitt <christopherschmitt@MacBookPro.ht.home>
2026-01-23 11:15:22 -0500

    v1.0.1 - Show version in header
    - Added version tag next to EasyDiet logo
    - Small, subtle font (0.55rem, 50% opacity)
    - Version synced across: VERSION file, manifest.json, sw.js, index.html

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
  * Provides offline functionality for the PWA
  */
 
-const CACHE_NAME = 'easydiet-1.0.0';
+const CACHE_NAME = 'easydiet-1.0.1';
 const STATIC_ASSETS = [
     '/',
     '/index.html',
