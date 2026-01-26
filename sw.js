0f338214eef02e5217ea0f442286dc80c7246fcf
Christopher Schmitt <christopherschmitt@Christophers-MacBook-Pro.local>
2026-01-26 07:18:46 -0500

    v1.12.4 - Fix: Reduce hydration for caffeinated beverages (mild diuretic effect)

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
  * Provides offline functionality for the PWA
  */
 
-const CACHE_NAME = 'easydiet-1.12.3';
+const CACHE_NAME = 'easydiet-1.12.4';
 const STATIC_ASSETS = [
     '/',
     '/index.html',
