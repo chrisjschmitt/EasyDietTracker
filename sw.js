16551aba6a06c4a012ce7dc8927f3fa63af3a333
Christopher Schmitt <christopherschmitt@Christophers-MacBook-Pro.local>
2026-01-26 17:48:57 -0500

    v1.16.0.3 - Fix tracker bar labels to be left-aligned on iOS

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
  * Provides offline functionality for the PWA
  */
 
-const CACHE_NAME = 'easydiet-1.16.0.2';
+const CACHE_NAME = 'easydiet-1.16.0.3';
 const STATIC_ASSETS = [
     '/',
     '/index.html',
